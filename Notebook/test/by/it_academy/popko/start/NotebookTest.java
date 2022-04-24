2e2f564397d4c0e677c0b84596c3289a6384a0a7
IgorPopko <99613099+IgorPopko@users.noreply.github.com>
2022-04-24 18:52:02 +0300

    Add files via upload

--- Notebook/test/by/it_academy/popko/start/NotebookTest.java
+++ Notebook/test/by/it_academy/popko/start/NotebookTest.java
@@ -1,5 +1,7 @@
 package by.it_academy.popko.start;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import by.it_academy.popko.logic.NotepadLogic;
@@ -8,10 +10,14 @@ import junit.framework.Assert;
 public class NotebookTest {
 
 	@Test
-	public void test() {
+	public void testGetFile() {
 		
-		
+		File expected = NotepadLogic.getFile();
 
+        File real = new File("E:\\Workspace\\Notebook\\notepadFile.txt");
+
+        Assert.assertEquals(expected, real);
+		
 	}
 
 }
